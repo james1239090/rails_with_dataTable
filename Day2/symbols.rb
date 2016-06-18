7bd03108ba610697d598f0b3d439a1d2f0d3a3c2
James <james1239090@gmail.com>
2016-06-18 21:23:34 +0800

    commit day2 for symbols

---
+++ Day2/symbols.rb
@@ -0,0 +1,10 @@
+ x = :james
+ y = :james
+ a = "james"
+ b = "james"
+
+ puts ("x==y :#{x == y}")
+ puts ("a==b :#{a == b}")
+ puts ("x.object_id == y.object_id :#{x.object_id == y.object_id}")
+ puts ("a.object_id == b.object_id :#{a.object_id == b.object_id}")
+
